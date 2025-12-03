18b1e9d202aa3abfae52c4b9bf0429258fde5b18
DuodecimAD <tonyxii@gmail.com>
2025-12-03 11:15:44 +0100

    fixed, forgot to leave loop when number is == 12 digits :'(

--- src/AoC_2025/days/day3.java
+++ src/AoC_2025/days/day3.java
@@ -4,56 +4,64 @@ import AoC_2025.io.Buffer;
 
 public class day3 {
     public static void main(String[] args) {
-        var file = "day3_ex.txt";
-//        var file = "day3.txt";
+//        var file = "day3_ex.txt";
+        var file = "day3.txt";
 //        part1(file);
         part2(file);
     }
 
     public static void part2(String file) {
-        try(var reader = Buffer.reader(file)) {
-            var total = 0;
-            var line = reader.readLine();
-            var length = line.length();
-            var number = 0;
-            while (line != null){
-                var numbers = "";
-                var lastNumber = 0;
-                var max = 0;
-                var pos = 0;
-                var valuei = 0;
-                var counter = 12;
-                var maxCounter = 12;
-                for (int k = 0; k < maxCounter; k++) {
-                    var LastIteration = length-counter;
-                    for(int i = pos; i <= LastIteration; i++){
-                        valuei = Integer.parseInt(String.valueOf(line.charAt(i)));
-                        if (valuei > max && i <= LastIteration){
-                            max = valuei;
-                            pos = i+1;
-                        }
 
-                        if(i == LastIteration) {
-                            counter--;
-                            numbers += max;
-                            max = 0;
-                        }
-                    }
-                    number = Integer.parseInt(numbers);
-                    total += number;
-                    System.out.println(number);
-                }
-                System.out.println("line number : " + number);
-                System.out.println("line total : " +total);
+        try(var reader = Buffer.reader(file)) {
+            long total = 0;
+
+            var line = reader.readLine();
+            while (line != null){
+
+                var number = getNumberForLine(line);
+
+                total += number;
+//                System.out.println("line number : " + number);
+//                System.out.println("line total : " + total);
 
                 line = reader.readLine();
             }
             System.out.println("total : " +total);
 
         } catch (Exception e) {
-
+            System.out.println(e.getMessage());
         }
 
+    }
+
+    private static long getNumberForLine(String line) {
+        var numbers = "";
+        var max = 0;
+        var pos = 0;
+        var length = line.length();
+        var numberLength = 12;
+        var counter = 12;
+        var LastIteration = 0;
+        for (int k = 0; k < length; k++) {
+            LastIteration = length-counter;
+            var valuei = 0;
+            for(int i = pos; i <= LastIteration; i++){
+                valuei = Integer.parseInt(String.valueOf(line.charAt(i)));
+                if (valuei > max && i <= LastIteration){
+                    max = valuei;
+                    pos = i+1;
+                }
+
+                if(i == LastIteration) {
+                    counter--;
+                    numbers += max;
+                    max = 0;
+                }
+            }
+//            System.out.println(numbers);
+            if(numbers.length() == numberLength) break;
+        }
+        return Long.parseLong(numbers);
     }
 
     public static void part1(String file) {
