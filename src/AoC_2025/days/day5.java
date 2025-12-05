1205c818eb44998ebb3722c210ea5cddcdbf83d9
DuodecimAD <tonyxii@gmail.com>
2025-12-05 11:28:32 +0100

    part1 done, smoke leaving my ears while thinking on part2

--- src/AoC_2025/days/day5.java
+++ src/AoC_2025/days/day5.java
@@ -2,9 +2,8 @@ package AoC_2025.days;
 
 import AoC_2025.io.Buffer;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
 
 public class day5 {
     public static void main(String[] args) {
@@ -15,66 +14,59 @@ public class day5 {
     }
 
     private static void part1(String file) {
-        var freshListLine = new HashSet<Long>();
+        var items = new HashSet<Long>();
+        var minMaxList = new ArrayList<Long[]>();
+        getItems(file, items);
+        buildMinMaxList(file, minMaxList);
+
+        var total = 0;
+        for (var minMax : minMaxList) {
+            var itemsConcurentModificationCopy = new HashSet<>(items);
+            for (var item : itemsConcurentModificationCopy) {
+                if(item >= minMax[0] && item <= minMax[1]){
+                    total++;
+                    items.remove(item);
+                }
+            }
+        }
+        System.out.println("total : " + total);
+    }
+
+    private static void buildMinMaxList(String file, ArrayList<Long[]> minMaxList) {
+        try(var reader = Buffer.reader(file)) {
+            var line = reader.readLine();
+            while (line != null){
+                if(line.isEmpty()){
+                    line = null;
+                } else {
+                    var minMax = line.split("-");
+                    var min = Long.parseLong(minMax[0]);
+                    var max = Long.parseLong(minMax[1]);
+                    minMaxList.add(new Long[]{min,max});
+                    line = reader.readLine();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void getItems(String file, HashSet<Long> freshListLine) {
         try(var reader = Buffer.reader(file)) {
             var line = reader.readLine();
             boolean items = false;
 
             while (line != null){
-
                 if(line.length() == 0){
                     items = true;
                     line = reader.readLine();
                     continue;
                 }
-
                 if(items){
-//                    var minMax = line.split("-");
-//                    var min = Long.parseLong(minMax[0]);
-//                    var max = Long.parseLong(minMax[1]);
-//                    for (long i = min; i <= max; i++) {
-//                        freshListLine.add(String.valueOf(i));
-//                    }
-
                     freshListLine.add(Long.parseLong(line));
                 }
-
-//                System.out.println(line);
-                line = reader.readLine();
-
-            }
-
-//            System.out.println(total);
-//            for (var item : freshListLine){
-//                System.out.println(item);
-//            }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-        try(var reader = Buffer.reader(file)) {
-            var line = reader.readLine();
-            int total = 0;
-            while (line != null){
-                if(line.length() == 0){
-                    return;
-                }
-                System.out.println(line);
-                var minMax = line.split("-");
-                var min = Long.parseLong(minMax[0]);
-                var max = Long.parseLong(minMax[1]);
-
-                for (var value : freshListLine) {
-                    if (value >= min && value <= max) {
-                        System.out.println(value + "yes");
-                        freshListLine.remove(value);
-                        total++;
-                    }
-                }
-
                 line = reader.readLine();
             }
-            System.out.println(total);
         } catch (Exception e) {
             System.out.println(e);
         }
