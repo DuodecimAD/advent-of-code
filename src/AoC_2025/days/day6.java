f033f79ff36e972796a360349aadca56b009c1f9
DuodecimAD <tonyxii@gmail.com>
2025-12-06 10:04:01 +0100

    day6 hard but still somewhat manageable for a noob like me that knows mostly only simple vanilla java methods, not pretty though

--- src/AoC_2025/days/day6.java
+++ src/AoC_2025/days/day6.java
@@ -6,7 +6,47 @@ public class day6 {
     public static void main(String[] args) {
 //        var file = "day6_ex.txt";
         var file = "day6.txt";
-        part1(file);
+//        part1(file);
+        part2(file);
+    }
+
+    private static void part2(String file) {
+        try(var reader = Buffer.reader(file)) {
+
+            var lines = reader.lines().toArray(String[]::new);
+            var operators = lines[lines.length-1].split(" +");
+            var counterOperator = operators.length - 1;
+            var length = lines[0].length();
+            long total = 0;
+            long totalLine = 0;
+            for (int i = length-1; i >= 0; i--) {
+                String number = String.valueOf(lines[0].charAt(i));
+                for (int j = 1; j < lines.length-1; j++) {
+                    number += lines[j].charAt(i);
+                }
+                if(number.isBlank()){
+                    total += totalLine;
+//                    System.out.println("totalLine : " + totalLine);
+                    totalLine = 0;
+                    counterOperator--;
+                    continue;
+                }
+//                number = number.replace(' ', '0');
+                if(totalLine != 0){
+                    totalLine = compute(totalLine, number.trim(), operators[counterOperator]);
+                } else {
+                    totalLine = Long.parseLong(number.trim());
+                }
+
+//                System.out.println(number);
+            }
+//            System.out.println("totalLine : " + totalLine);
+            total += totalLine;
+            System.out.println("total : " + total);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     private static void part1(String file) {
@@ -27,23 +67,19 @@ public class day6 {
             }
             System.out.println("total: " + total);
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
     }
 
     private static long compute(long totalLone, String numberS, String operator) {
 
         var number = Integer.parseInt(numberS);
-        switch (operator) {
-            case "+":
-                return totalLone + number;
-            case "*":
-                return totalLone * number;
-            case "-":
-                return totalLone - number;
-            case "/":
-                return totalLone / number;
-        }
-        throw new RuntimeException("Unknown operator : " + operator);
+        return switch (operator) {
+            case "+" -> totalLone + number;
+            case "*" -> totalLone * number;
+            case "-" -> totalLone - number;
+            case "/" -> totalLone / number;
+            default -> throw new RuntimeException("Unknown operator : " + operator);
+        };
     }
 }
